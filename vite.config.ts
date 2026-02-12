a75206440656eca61341612e96e4f87700156d03
tingminitime <e0610k@gmail.com>
2026-02-12 15:21:29 +0800

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/simple-weather/',
+    base: '/simple-weather-demo/',
 
     resolve: {
       alias: {
